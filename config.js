7f1200da32310cf6c70e65d69b95f2c4b3e7a5f6
jkavya491 <102381329+jkavya491@users.noreply.github.com>
2022-11-03 20:58:00 +0530

    Update config.js

--- config.js
+++ config.js
@@ -71,9 +71,9 @@ const CONFIG = {
 		},
 		{
 			id: '4',
-			name: 'Calendar',
-			icon: 'calendar',
-			link: 'https://calendar.google.com/calendar/r',
+			name: 'Spotify',
+			icon: 'headphones',
+			link: 'https://open.spotify.com/',
 		},
 		{
 			id: '5',
