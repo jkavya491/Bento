2545905678419a38ef4c65febd93b26bf37c9a53
jkavya491 <102381329+jkavya491@users.noreply.github.com>
2023-04-01 13:22:45 +0530

    Update config.js

--- config.js
+++ config.js
@@ -26,7 +26,7 @@ const CONFIG = {
 	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
 
 	// Weather
-	weatherKey: '1254661', // Write here your API Key
+	weatherKey: 'https://openweathermap.org/city/1254661', // Write here your API Key
 	weatherIcons: 'OneDark', // 'Onedark', 'Nord', 'Dark', 'White'
 	weatherUnit: 'C', // 'F', 'C'
 	language: 'en', // More languages in https://openweathermap.org/current#multi
